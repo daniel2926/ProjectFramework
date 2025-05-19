b5768122316ea4211a53f80e239f8e50c6f83e4a
MelaniusDaniarta <MelaniusDaniarta.com>
2025-05-19 19:34:15 +0700

    add baru

---
+++ base-final-project/src/main/java/ac/jiu/bobby/project/controller/TestController.java
@@ -0,0 +1,21 @@
+package ac.jiu.bobby.project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+
+    @GetMapping("/public")
+    public String publicEndpoint() {
+        return "This is a public endpoint.";
+    }
+
+    @GetMapping("/private")
+    public String privateEndpoint() {
+        return "This is a secured endpoint.";
+    }
+}
+
